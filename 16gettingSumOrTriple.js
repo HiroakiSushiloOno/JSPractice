0ab14f9d66d256cdd585a1054cb820ab234290f0
HiroakiSushiloOno <sushilo0613@gmail.com>
2018-10-17 14:41:04 -0700

    I added 16gettingSumOrTriple.js

---
+++ 16gettingSumOrTriple.js
@@ -0,0 +1,10 @@
+function calc(number1, number2){
+    if(number1 == number2){
+        console.log(number1 * 3);
+    }else{
+        console.log(number1 + number2);
+    }
+}
+
+console.log(calc(20, 21));
+console.log(calc(20, 20));
