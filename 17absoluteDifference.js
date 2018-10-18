d7bbd1888e92dc6768ab1d2a8fffb0c40486b76c
HiroakiSushiloOno <sushilo0613@gmail.com>
2018-10-17 17:16:23 -0700

    I added 17absoluteDifference.js

---
+++ 17absoluteDifference.js
@@ -0,0 +1,8 @@
+let number1 = prompt("type a number");
+const number = 19;
+
+if(number > number1){
+    console.log(Math.abs(number - number1));
+}else{
+    console.log(Math.abs(number1 - number));
+}
