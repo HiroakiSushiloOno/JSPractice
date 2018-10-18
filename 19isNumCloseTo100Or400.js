95eacd60994768859626dec1adc5bd37cdf6f793
HiroakiSushiloOno <sushilo0613@gmail.com>
2018-10-17 17:29:50 -0700

    I added js exercise 19

---
+++ 19isNumCloseTo100Or400.js
@@ -0,0 +1,9 @@
+let number = prompt("type a number");
+
+if(number >= 80 && number <= 120){
+    console.log("it's close to 100");
+}else if(number >= 380 && number <= 420){
+    console.log("it's close to 400");
+}else{
+    console.log("it doesn't meet the condition.");
+}
