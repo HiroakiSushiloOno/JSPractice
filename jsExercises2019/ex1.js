2f080f808befd9089960b837fdda59356c91e46c
HiroakiOno <sushilo0613@gmail.com>
2019-01-11 12:19:25 -0800

    added a js practice

--- jsExercises2019/ex1.js
+++ jsExercises2019/ex1.js
@@ -1,6 +1,18 @@
-let num = 881122;
+//using regurar function
 const reverseNum = (num) => {
   num = num + "";
   console.log(num.split("").reverse(num).join(""));
 }
-reverseNum(num);
+reverseNum(1234);
+
+//using class instead
+const reverseNum2 = class{
+  constructor(num){
+    this.num = num + "";
+  }
+  reverse(){
+    return this.num.split("").reverse(this.num).join("");
+  }
+}
+const testInstance = new reverseNum2(1234);
+console.log(testInstance.reverse());
---
+++ jsExercises2019/ex3b.js
@@ -0,0 +1,49 @@
+const possiblePatterns =  class {
+  constructor(str){
+    this.arr = str.split("");
+    this.combi = [];
+    this.temp = "";
+    this.slent = Math.pow(2, this.arr.length);
+    this.palindromeNum = 0;
+  }
+  showPatterns(){
+    for(let i = 0; i < this.slent; i++){
+      this.temp = "";
+      for(let j = 0; j < this.arr.length; j++){
+        if((i & Math.pow(2, j))){
+          this.temp += this.arr[j];
+        }
+      }
+      if(this.temp !== ""){
+        this.combi.push(this.temp);
+        let s = new checkPalindrome(this.temp);
+        if(s.check()){
+          this.palindromeNum++;
+        }
+
+      }
+    }
+    console.log(this.combi.join("\n"));
+    console.log("there ware " + this.palindromeNum + "palindromes!");
+  }
+}
+
+const checkPalindrome = class {
+  constructor(str){
+    this.str = str;
+    this.reverse = str.split("").reverse(str).join("");
+  }
+  check(){
+    if(this.str.length > 1){
+      if(this.str === this.reverse){
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }
+}
+const testClass = new possiblePatterns("madam");
+testClass.showPatterns();
